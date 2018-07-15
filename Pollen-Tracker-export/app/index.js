28d799f4df2b50ee384dc0e2f7d0a39391d9ca88
Ethan Schaffer <ethan.schaffer@data.world>
2018-07-15 09:39:01 -0500

    UI improvement, scaling for bar chart

--- Pollen-Tracker-export/app/index.js
+++ Pollen-Tracker-export/app/index.js
@@ -23,9 +23,26 @@ clock.granularity = "seconds";
 // Get a handle on the <text> element
 const myLabel = document.getElementById("location");
 const circle = document.getElementById("circle");
+const oneDay = document.getElementById("tomorrow");
+const twoDay = document.getElementById("two day");
+const threeDay = document.getElementById("three day");
+const weekDayOne = document.getElementById("day one");
+const weekDayTwo = document.getElementById("day two");
+const weekDayThree = document.getElementById("day three");
 
-function setColor(color) {
-  circle.style.fill = color
+
+function setColor(item, color) {
+  item.style.fill = color
+}
+
+function setColorIndex(item, index){
+  if(index < 4.9) {
+    setColor(item, "green");
+  } else if(index < 7.3) {
+    setColor(item, "yellow");
+  } else {
+    setColor(item, "red");
+  }
 }
 
 messaging.peerSocket.onmessage = function(evt) {
@@ -38,19 +55,44 @@ messaging.peerSocket.onmessage = function(evt) {
   document.getElementById("pType").text = "Pollen Type: "+pollenType.substring(1,pollenType.length-1); 
   var index = parseInt(pollenIndex, 10);
   console.log(index)
-  if(index < 4.9) {
-    setColor("green");
-  } else if(index < 7.3) {
-    setColor("yellow");
-  } else {
-    setColor("red");
-  }
+
+  setColorIndex(circle, index)
+  var maxHeight = 70
+  
+  var oneDayNumber = evt['data'][0].Tomorrow
+  var twoDayNumber = evt['data'][0].TwoDays
+  var threeDayNumber = evt['data'][0].ThreeDays
+  
+  var maxNumber = Math.max(oneDayNumber, oneDayNumber, oneDayNumber)
+  
+  oneDay.height = maxHeight * oneDayNumber / maxNumber
+  twoDay.height = maxHeight * twoDayNumber / maxNumber
+  threeDay.height = maxHeight * threeDayNumber / maxNumber
+  
+  oneDay.y = 200 - oneDay.height
+  twoDay.y = 200 - twoDay.height
+  threeDay.y = 200 - threeDay.height
+ 
+  oneDay.style.fill = "white";
+  twoDay.style.fill = "white";
+  threeDay.style.fill = "white";
 }
 let myClock = document.getElementById("myClock");
 
 clock.granularity = 'seconds'; // seconds, minutes, hours
 
+function weekDay(day) {  
+  var days = ["S", "M", "T", "W", "T", "F", "S"]
+  return days[day]
+}
+
 clock.ontick = function(evt) {
+  var day = evt.date.getDay()
+  console.log("day =" + day)
+  weekDayOne.text = weekDay(day)
+  weekDayTwo.text = weekDay(day+1)
+  weekDayThree.text = weekDay(day+2)
+  console.log(weekDayOne.text)
   myClock.text = ("0" + evt.date.getHours()).slice(-2) + ":" +
                       ("0" + evt.date.getMinutes()).slice(-2) + ":" +
                       ("0" + evt.date.getSeconds()).slice(-2);
